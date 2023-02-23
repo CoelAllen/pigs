7da58eea80a06e4e433755fafb7829b7b3f51dff
Coel Allen <coelallen@gmail.com>
2023-02-23 14:34:27 -0700

    volunteer form blocked out

--- src/components/VolunteerForm/Form.js
+++ src/components/VolunteerForm/Form.js
@@ -35,49 +35,70 @@ export default function Form() {
             <div className={styles["form--inputs"]}>
               <div className={styles["form--input"]}>
                 <label htmlFor="name">Name:</label>
-                <input type="text" placeholder="Name" name="name" />
+                <input
+                  className={styles["input"]}
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                />
               </div>
               <div className={styles["form--input"]}>
                 <label htmlFor="email">Email:</label>
-                <input type="email" placeholder="Email" name="email" />
+                <input
+                  className={styles["input"]}
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                />
               </div>
               <div className={styles["form--input"]}>
                 <label htmlFor="phone">Phone Number:</label>
-                <input type="tel" placeholder="Phone Number" name="phone" />
+                <input
+                  className={styles["input"]}
+                  type="tel"
+                  placeholder="Phone Number"
+                  name="phone"
+                />
               </div>
               <h4>Please select which days you are available:</h4>
               <div className={styles["input--week"]}>
-                <div>
+                <div className={styles["input--day"]}>
                   <label htmlFor="sunday">Sunday</label>
                   <input type="checkbox" name="sunday" />
                 </div>
-                <div>
+                <div className={styles["input--day"]}>
                   <label htmlFor="monday">Monday</label>
                   <input type="checkbox" name="monday" />
                 </div>
-                <div>
+                <div className={styles["input--day"]}>
                   <label htmlFor="tuesday">Tuesday</label>
                   <input type="checkbox" name="tuesday" />
                 </div>
-                <div>
+                <div className={styles["input--day"]}>
                   <label htmlFor="wednesday">Wednesday</label>
                   <input type="checkbox" name="wednesday" />
                 </div>
-                <div>
+                <div className={styles["input--day"]}>
                   <label htmlFor="thursday">Thursday</label>
                   <input type="checkbox" name="thursday" />
                 </div>
-                <div>
+                <div className={styles["input--day"]}>
                   <label htmlFor="friday">Friday</label>
                   <input type="checkbox" name="friday" />
                 </div>
-                <div>
+                <div className={styles["input--day"]}>
                   <label htmlFor="saturday">Saturday</label>
                   <input type="checkbox" name="saturday" />
                 </div>
               </div>
-              <label htmlFor="description">A little about yourself:</label>
-              <input type="textarea" name="description" />
+              <div className={styles["input--textarea"]}>
+                <label htmlFor="description">A little about yourself:</label>
+                <textarea rows={5} cols={50} name="description"></textarea>
+              </div>
+              <div className={styles["form--buttons"]}>
+                <div className={styles["modal--submit"]}>Submit</div>
+                <div className={styles["modal--close"]}>Close</div>
+              </div>
             </div>
           </div>
         </form>
